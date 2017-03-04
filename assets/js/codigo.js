e3ffa6201350f847d8ce8a7e5187b04622d6e10b
Nadia Cuadros <november138@hotmail.com>
2017-03-03 19:33:22 -0500

    Lista de trabajadores de Laboratoria

---
+++ assets/js/codigo.js
@@ -0,0 +1,8 @@
+var Trabajador = [ { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+ { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+ { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+ { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+ { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ]
+Trabajador.forEach(e){
+  return nombre;
+}
